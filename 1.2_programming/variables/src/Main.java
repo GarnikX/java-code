d25dffd8e95381f651d5858189fa72bf314d2eac
Garnik Khachatryan <garniktester@gmail.com>
2021-12-14 00:21:37 +0400

    Money transfer

--- 1.2_programming/variables/src/Main.java
+++ 1.2_programming/variables/src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
-  public static void main(String[] args) {
-    int price = 150;
-    int count = 5;
-    int total = price * count;
-    System.out.println(total);
-  }
+    public static void main(String[] args) {
+        int customer_account = 2_000_000_000;
+        int money_transfer = 500_000_000;
+        int total = customer_account + money_transfer;
+        System.out.println(total);
+    }
 }
-
